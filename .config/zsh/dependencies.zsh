8e7371fe1229d72989740f5f04c2ae4af2a6138d
rory <hamster2020@live.com>
2021-07-30 18:20:53 +0100

    moved nvm install location

--- .config/zsh/dependencies.zsh
+++ .config/zsh/dependencies.zsh
@@ -1,5 +1,9 @@
-if ! [ -d ${HOME}/.nvm ]; then
-        wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-        source $HOME/.zshrc
-        nvm install --lts
+if ! [ -d $NVM_DIR ]; then
+    mkdir $NVM_DIR
+    XDG_OLD=$XDG_CONFIG_HOME
+    XDG_CONFIG_HOME=$NVM_DIR
+    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+    XDG_CONFIG_HOME-$XDG_OLD
+    source $HOME/.zshrc
+    nvm install --lts
 fi
