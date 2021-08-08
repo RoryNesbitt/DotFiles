396ed6b55d61d98442b855524480bb86a9f6d4c6
rory <hamster2020@live.com>
2021-08-08 12:22:09 +0100

    Added

---
+++ .config/zsh/xdg.zsh
@@ -0,0 +1,12 @@
+#If anyone can tell me why XDG doesn't just work it would be greatly appreciated
+#in the meantime:
+#
+XDG_DESKTOP_HOME="$HOME/Desktop"
+XDG_DOWNLOAD_HOME="$HOME/Downloads"
+XDG_TEMPLATES_HOME="$HOME/Templates"
+XDG_PUBLICSHARE_HOME="$HOME/Public"
+XDG_DOCUMENTS_HOME="$HOME/Documents"
+XDG_MUSIC_HOME="$HOME/Music"
+XDG_PICTURES_HOME="$HOME/Pictures"
+XDG_VIDEOS_HOME="$HOME/Videos"
+XDG_CONFIG_HOME="$HOME/.config"
