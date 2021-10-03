5b567e5449e6265a4b695680ce082646fd4d8875
rory <ranesbitt@live.co.uk>
2021-10-04 00:44:11 +0100

    Added clipit

--- .bin/desktop.sh
+++ .bin/desktop.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -Syyu --noconfirm kitty vivaldi discord steam docker pulseaudio pavucontrol tmux neovim nodejs npm
 
-yay -S --noconfirm realvnc-vnc-viewer openconnect
+yay -S --noconfirm realvnc-vnc-viewer openconnect teams
 
 ### Docker
 sudo groupadd docker
