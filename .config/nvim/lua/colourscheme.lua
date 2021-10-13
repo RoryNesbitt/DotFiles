747e0e0bbbcdaee4b66e560a93f8635a91fdb0b3
rory <ranesbitt@live.co.uk>
2021-10-13 22:21:44 +0100

    Fixed clear background

--- .config/nvim/lua/colourscheme.lua
+++ .config/nvim/lua/colourscheme.lua
@@ -1,8 +1,6 @@
 vim.cmd('colorscheme gruvbox')
 --vim.g.airline_theme = 'gruvbox'
 
-vim.cmd([[
-highlight ColorColumn ctermbg=8
-highlight Normal ctermbg=none
-highlight NonText ctermbg=none
-]])
+vim.cmd[[au VimEnter * highlight ColorColumn ctermbg=8]]
+vim.cmd[[au VimEnter * highlight Normal ctermbg=none]]
+vim.cmd[[au VimEnter * highlight NonText ctermbg=none]]
