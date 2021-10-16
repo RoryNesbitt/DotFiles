b188e7bc1c0bf35c1f632b4f1b141f82efadd028
rory <ranesbitt@live.co.uk>
2021-10-16 01:15:28 +0100

    Added FiraCode

--- .bin/desktop.sh
+++ .bin/desktop.sh
@@ -4,6 +4,10 @@ sudo pacman -Syyu --noconfirm kitty vivaldi discord steam docker pulseaudio pavu
 
 yay -S --noconfirm realvnc-vnc-viewer openconnect teams
 
+###Nerd Font
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Fira Code Regular Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
+
 ### Docker
 sudo groupadd docker
 sudo usermod -aG docker $USER
