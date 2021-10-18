7d981c1edb50124484f806a331556126c8a5e8ae
rory <ranesbitt@live.co.uk>
2021-10-18 20:59:38 +0100

    Spring cleaning

--- .bin/rightPortrait.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI-0 --mode 1920x1080 --pos 4480x0 --rotate right --output DP-0 --primary --mode 2560x1080 --pos 1920x77 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-1 --mode 1920x1080 --pos 0x77 --rotate normal --output USB-C-0 --off
