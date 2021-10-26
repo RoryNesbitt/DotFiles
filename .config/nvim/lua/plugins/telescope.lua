b0c9b98522640ed5db7c41ac8fd8e68e0a0c3d5a
rory <ranesbitt@live.co.uk>
2021-10-26 22:11:33 +0100

    Removed fzy-native since it likes to have a hissy fit on a pi

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -25,7 +25,7 @@ require('telescope').setup {
     }
 }
 
-require('telescope').load_extension('fzy_native')
+--require('telescope').load_extension('fzy_native')
 
 local M = {}
 M.search_dotfiles = function()
