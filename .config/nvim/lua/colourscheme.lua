d30d1a296b951526885fafcc51649e80929b057a
rory <ranesbitt@live.co.uk>
2021-10-29 17:54:26 +0100

    Fixed background transparency

--- .config/nvim/lua/colourscheme.lua
+++ .config/nvim/lua/colourscheme.lua
@@ -2,5 +2,5 @@ vim.cmd('colorscheme gruvbox')
 --vim.g.airline_theme = 'gruvbox'
 
 vim.cmd[[au VimEnter * highlight ColorColumn ctermbg=8]]
-vim.cmd[[au VimEnter * highlight Normal ctermbg=none]]
-vim.cmd[[au VimEnter * highlight NonText ctermbg=none]]
+vim.cmd[[au VimEnter * highlight Normal guibg=none]]
+vim.cmd[[au VimEnter * highlight NonText guibg=none]]
