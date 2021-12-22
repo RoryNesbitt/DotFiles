d0f6d50f81f2ff198612fc96e1eb219527e14afe
rory <ranesbitt@live.co.uk>
2021-12-22 21:13:38 +0000

    Removed manual installation of lsp servers

--- .local/scripts/desktop.sh
+++ .local/scripts/desktop.sh
@@ -3,18 +3,6 @@
 sudo pacman -Syyu --noconfirm kitty vivaldi discord steam docker pulseaudio pavucontrol tmux neovim nodejs npm wine yarn python-pip dash xclip fzf
 yay -S --noconfirm realvnc-vnc-viewer openconnect teams nerd-fonts-fira-code
 
-### LSP servers
-sudo npm install -g @tailwindcss/language-server
-yarn global add ansible-language-server yaml-language-server vscode-langservers-extracted emmet-ls vim-language-server typescript typescript-language-server
-pip3 install 'python-lsp-server[all]'
-
-#bashls
-#clangd
-#dockerls
-#jsonls
-#sumneko_lua
-#texlab
-
 sudo rm /bin/sh
 sudo ln /bin/dash /bin/sh
 
