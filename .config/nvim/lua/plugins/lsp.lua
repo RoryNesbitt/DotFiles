45c10ec0868ebdeae511ac3a9b74b4bea673e0e7
rory <ranesbitt@live.co.uk>
2021-12-23 00:19:58 +0000

    updated tab width in the file

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -30,44 +30,44 @@ end
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 local servers = {
-	'ansiblels',
-	'bashls',
-	'cssls',
-	'dockerls',
-	'html',
-	'jsonls',
-	'pylsp',
-	'tailwindcss',
-	'texlab',
-	'tsserver',
-	'vimls',
-	'yamlls'
+  'ansiblels',
+  'bashls',
+  'cssls',
+  'dockerls',
+  'html',
+  'jsonls',
+  'pylsp',
+  'tailwindcss',
+  'texlab',
+  'tsserver',
+  'vimls',
+  'yamlls'
 }
 for _, lsp in ipairs(servers) do
-    require('lspconfig')[lsp].setup {
-        on_attach = on_attach,
-        capabilities = capabilities,
-        flags = {
-            debounce_text_changes = 150,
-        }
+  require('lspconfig')[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = {
+      debounce_text_changes = 150,
     }
+  }
 end
 
 require('lspconfig').clangd.setup{
-    on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { "c", "cpp", "objc", "objcpp", "ino" }
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "c", "cpp", "objc", "objcpp", "ino" }
 }
 
 require'lspconfig/configs'.ls_emmet = {
-    default_config = {
-        cmd = { 'ls_emmet', '--stdio' };
-        filetypes = { 'html', 'css', 'scss', 'jsx' }; -- Add the languages you use, see language support
-        root_dir = function(_)
-            return vim.loop.cwd()
-        end;
-        settings = {};
-    };
+  default_config = {
+    cmd = { 'ls_emmet', '--stdio' };
+    filetypes = { 'html', 'css', 'scss', 'jsx' }; -- Add the languages you use, see language support
+    root_dir = function(_)
+      return vim.loop.cwd()
+    end;
+    settings = {};
+  };
 }
 
 local system_name = 'Linux'
@@ -77,28 +77,28 @@ local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 require'lspconfig'.sumneko_lua.setup {
-    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
-    settings = {
-	Lua = {
-	    runtime = {
-		-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-		version = 'LuaJIT',
-		-- Setup your lua path
-		path = runtime_path,
-	    },
-	    diagnostics = {
-		-- Get the language server to recognize the `vim` global
-		globals = {'vim'},
-	    },
-	    workspace = {
-		-- Make the server aware of Neovim runtime files
-		library = vim.api.nvim_get_runtime_file("", true),
-		checkThirdParty = false
-	    },
-		-- Do not send telemetry data containing a randomized but unique identifier
-	    telemetry = {
-		enable = false,
-	    },
-	},
+  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
     },
+  },
 }
