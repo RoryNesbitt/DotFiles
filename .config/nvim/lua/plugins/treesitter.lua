1b8779d1acd17c8b4a574bfe1a2d6dcc2eed0de0
rory <ranesbitt@live.co.uk>
2021-12-23 01:00:57 +0000

    fixed treesitter after pcall broke it. I don't understand why it broke

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -9,21 +9,15 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
 	update_in_insert = true,
     }
 )
-local status_ok, nvim_treesitter = pcall(require, 'nvim_treesitter')
+local status_ok, configs = pcall(require, 'nvim-treesitter.configs')
 if not status_ok then
   return
 end
 
-nvim_treesitter.configs.setup {
-  ensure_installed = 'maintained', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  --ignore_install = { "javascript" }, -- List of parsers to ignore installing
+configs.setup {
+  ensure_installed = 'maintained',
   highlight = {
-    enable = true,              -- false will disable the whole extension
-    --disable = { "c", "rust" },  -- list of language that will be disabled
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
+    enable = true,
     additional_vim_regex_highlighting = false,
   },
   autotag = {
@@ -31,10 +25,8 @@ nvim_treesitter.configs.setup {
   },
   rainbow = {
     enable = true,
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
-    -- termcolors = {} -- table of colour name strings
+    extended_mode = true,
+    max_file_lines = nil,
   },
   autopairs = {
     enable = true,
