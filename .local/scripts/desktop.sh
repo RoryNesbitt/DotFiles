fd3486983fdb29a84a26d0dc2ed2e4a4a90544fa
rory <ranesbitt@live.co.uk>
2022-01-02 20:19:11 +0000

    Changed neovim git pull to use ssh

--- .local/scripts/desktop.sh
+++ .local/scripts/desktop.sh
@@ -7,7 +7,7 @@ sudo rm /bin/sh
 sudo ln /bin/dash /bin/sh
 
 ### Neovim
-git clone https://github.com/rorynesbitt/neovim ~/.config/nvim
+git clone git@github.com:rorynesbitt/neovim ~/.config/nvim
 
 ### Docker
 sudo groupadd docker
