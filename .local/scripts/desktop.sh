0d827118a2f3a41382431d915c0a9e27d9c68cd1
rory <ranesbitt@live.co.uk>
2022-01-03 00:49:32 +0000

    Added null-ls dependancies

--- .local/scripts/desktop.sh
+++ .local/scripts/desktop.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
-sudo pacman -Syyu --noconfirm kitty vivaldi discord steam docker pulseaudio pavucontrol tmux neovim nodejs npm wine yarn python-pip dash xclip fzf ripgrep
+sudo pacman -Syyu --noconfirm kitty vivaldi discord steam docker pulseaudio pavucontrol tmux neovim nodejs npm wine yarn python-pip dash xclip fzf ripgrep cmake luarocks
 yay -S --noconfirm realvnc-vnc-viewer openconnect teams nerd-fonts-fira-code
 
 sudo rm /bin/sh
 sudo ln /bin/dash /bin/sh
 
-### Neovim
+## Neovim
 git clone git@github.com:rorynesbitt/neovim ~/.config/nvim
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+### null-ls formatters
+luarocks install --server=https://luarocks.org/dev luaformatter
 
-### Docker
+## Docker
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
 sudo systemctl enable docker
 sudo systemctl start docker
 
-### tmux
+## Tmux
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 
 exit
