5b6db7f5774a3f0303099b080e0593b77736e334
rory <ranesbitt@live.co.uk>
2022-01-09 22:39:34 +0000

    added stylua and black

--- .local/scripts/desktop.sh
+++ .local/scripts/desktop.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo pacman -Syyu --noconfirm kitty vivaldi discord steam docker pulseaudio pavucontrol tmux neovim nodejs npm wine yarn python-pip dash xclip fzf ripgrep cmake luarocks
-yay -S --noconfirm realvnc-vnc-viewer openconnect teams nerd-fonts-fira-code
+sudo pacman -Syyu --noconfirm kitty vivaldi discord steam docker pulseaudio pavucontrol tmux neovim nodejs npm wine yarn python-pip dash xclip fzf ripgrep cmake
+yay  --noconfirm realvnc-vnc-viewer openconnect teams nerd-fonts-fira-code
 
 sudo rm /bin/sh
 sudo ln /bin/dash /bin/sh
@@ -10,8 +10,9 @@ sudo ln /bin/dash /bin/sh
 git clone git@github.com:rorynesbitt/neovim ~/.config/nvim
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 ### null-ls formatters
-luarocks install --server=https://luarocks.org/dev luaformatter
+yay --noconfirm stylua
 yarn global add prettier
+pip install black
 
 ## Docker
 sudo groupadd docker
