13023d51ff2326c5a17df4e2baa42d1f92aba2fe
rory <ranesbitt@live.co.uk>
2022-01-14 15:08:59 +0000

    get prettier from pacman instead of yarn

--- .local/scripts/desktop.sh
+++ .local/scripts/desktop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pacman -Syyu --noconfirm kitty vivaldi discord steam docker pulseaudio pavucontrol tmux neovim nodejs npm wine yarn python-pip dash xclip fzf ripgrep cmake
+sudo pacman -Syu --noconfirm kitty vivaldi discord steam docker pulseaudio pavucontrol tmux neovim nodejs npm wine yarn python-pip dash xclip fzf ripgrep cmake
 yay  --noconfirm realvnc-vnc-viewer openconnect teams nerd-fonts-fira-code
 
 sudo rm /bin/sh
@@ -10,8 +10,8 @@ sudo ln /bin/dash /bin/sh
 git clone git@github.com:rorynesbitt/neovim ~/.config/nvim
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 ### null-ls formatters
+sudo --noconfirm pacman -S prettier
 yay --noconfirm stylua
-yarn global add prettier
 pip install black
 
 ## Docker
