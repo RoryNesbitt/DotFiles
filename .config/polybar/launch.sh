437ad5346d028abf99cca21e011fec1387f07ba8
RoryNesbitt <ranesbitt@live.co.uk>
2023-05-23 12:36:07 +0100

    handle multi monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,7 +8,15 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+# polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 # polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+for m in $(polybar --list-monitors | grep primary | cut -d":" -f1); do
+  MONITOR=$m polybar --reload bar1 &
+  echo $m
+done
+for m in $(polybar --list-monitors | grep -v primary | cut -d":" -f1); do
+  echo $m
+  MONITOR=$m polybar --reload bar1 & # TODO: bar2
+done
 
 echo "Bars launched..."
