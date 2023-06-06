70f2b28cd6d950e885d710c49cc9bcdbee8146fa
RoryNesbitt <ranesbitt@live.co.uk>
2023-06-06 15:48:06 +0100

    Seperated main/side bar configs

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,17 +6,16 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 # polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 # polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 for m in $(polybar --list-monitors | grep primary | cut -d":" -f1); do
-  MONITOR=$m polybar --reload bar1 &
+  MONITOR=$m polybar --reload main &
   echo $m
 done
 for m in $(polybar --list-monitors | grep -v primary | cut -d":" -f1); do
   echo $m
-  MONITOR=$m polybar --reload bar1 & # TODO: bar2
+  MONITOR=$m polybar --reload side &
 done
 
 echo "Bars launched..."
