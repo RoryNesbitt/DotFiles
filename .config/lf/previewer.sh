35ab935536f2163d88056193ba83735c974e1871
Rory <ranesbitt@live.co.uk>
2023-08-30 10:41:18 +0100

    removed neovim from the previewer

--- .config/lf/previewer.sh
+++ .config/lf/previewer.sh
@@ -39,9 +39,9 @@ case "$(basename "$file" | tr '[:upper:]' '[:lower:]')" in
     preview "$thumbnail" "$@"
     ;;
   *)
-    if command -v nvim > /dev/null 2>&1; then
-        nvim -c "so ~/.config/lf/nvim.lua" "$file"
-    elif command -v bat > /dev/null 2>&1; then
+    # if command -v nvim > /dev/null 2>&1; then
+    #     nvim -c "so ~/.config/lf/nvim.lua" "$file"
+    if command -v bat > /dev/null 2>&1; then
         bat -pf "$file"
     else
         cat "$file"
