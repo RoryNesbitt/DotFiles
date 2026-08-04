8fbc9a46334313ad3c01147c6c84c22de0e679de
Rory Nesbitt <ranesbitt@live.co.uk>
2026-08-04 15:34:50 +0100

    moved to swaybg

--- .config/hypr/land/autostart.lua
+++ .config/hypr/land/autostart.lua
@@ -3,16 +3,9 @@
 -----------------
 
 hl.on("hyprland.start", function()
-  -- Set a random wallpaper via hyprpaper template
-  local home = os.getenv("HOME")
-  local new_wall = io.popen(string.format("find %s/Documents/wallpapers/* -type f | shuf -n 1", home)):read("*l")
-  os.execute(string.format(
-    "sed -e 's~<wp>~%s~g' %s/.config/hypr/paper/template.conf > %s/.config/hypr/hyprpaper.conf",
-    new_wall, home, home
-  ))
-
+  hl.exec_cmd('new_wall=$(find $HOME/Documents/wallpapers/* -type f | shuf -n 1) && \
+              swaybg -c 000000 -o DP-1 -i "$new_wall" -o eDP-1 -i "$new_wall" &')
   hl.exec_cmd("waybar &")
-  hl.exec_cmd("hyprpaper &")
   hl.exec_cmd("steam -silent &")
   hl.exec_cmd("ferdium &")
 end)
